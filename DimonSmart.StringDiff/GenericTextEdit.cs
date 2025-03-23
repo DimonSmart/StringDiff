c06cd40ce718d5e565cb09c4b5e202d1cce31770
Dmitry Dorogoy <Dmitrii_Dorogoi@epam.com>
2025-03-23 22:43:32 +0100

    WIP

--- DimonSmart.StringDiff/GenericTextEdit.cs
+++ DimonSmart.StringDiff/GenericTextEdit.cs
@@ -1,22 +1,17 @@
 namespace DimonSmart.StringDiff;
 
-public class GenericTextEdit<T>
+public readonly struct GenericTextEdit<T>
 {
-    private readonly ReadOnlyMemory<T> _deletedTokensMemory;
-    private readonly ReadOnlyMemory<T> _insertedTokensMemory;
-    private IReadOnlyList<T>? _deletedTokens;
-    private IReadOnlyList<T>? _insertedTokens;
-
     public int StartPosition { get; }
-    public IReadOnlyList<T> DeletedTokens => _deletedTokens ??= _deletedTokensMemory.ToArray();
-    public IReadOnlyList<T> InsertedTokens => _insertedTokens ??= _insertedTokensMemory.ToArray();
+    public ReadOnlyMemory<T> DeletedTokens { get; }
+    public ReadOnlyMemory<T> InsertedTokens { get; }
     internal ReadOnlyMemory<T> SourceTokens { get; }
 
     internal GenericTextEdit(int startPosition, ReadOnlyMemory<T> deletedTokens, ReadOnlyMemory<T> insertedTokens, ReadOnlyMemory<T> sourceTokens)
     {
         StartPosition = startPosition;
-        _deletedTokensMemory = deletedTokens;
-        _insertedTokensMemory = insertedTokens;
+        DeletedTokens = deletedTokens;
+        InsertedTokens = insertedTokens;
         SourceTokens = sourceTokens;
     }
 }
--- DimonSmart.StringDiff/Internal/GenericTextEditExtensions.cs
+++ DimonSmart.StringDiff/Internal/GenericTextEditExtensions.cs
@@ -5,10 +5,10 @@ internal static class GenericTextEditExtensions
     internal static TextEdit ToStringEdit<T>(this GenericTextEdit<T> edit)
     {
         var offset = edit.StartPosition > 0 && edit.SourceTokens.Length > 0
-            ? edit.SourceTokens.Slice(0, edit.StartPosition).Span.ToArray().Sum(t => t?.ToString()?.Length ?? 0)
+            ? edit.SourceTokens.Slice(0, edit.StartPosition).ToArray().Sum(t => t?.ToString()?.Length ?? 0)
             : 0;
-        var length = edit.DeletedTokens.Count > 0 ? edit.DeletedTokens.Sum(t => t?.ToString()?.Length ?? 0) : 0;
-        var insertedText = string.Concat(edit.InsertedTokens.Select(t => t?.ToString() ?? ""));
+        var length = edit.DeletedTokens.Length > 0 ? edit.DeletedTokens.ToArray().Sum(t => t?.ToString()?.Length ?? 0) : 0;
+        var insertedText = String.Concat(edit.InsertedTokens.ToArray().Select(t => t?.ToString() ?? ""));
         return new TextEdit(offset, length, insertedText);
     }
 }
